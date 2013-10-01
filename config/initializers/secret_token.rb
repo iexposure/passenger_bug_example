053f2bd45ac77863468e4037b1c9c3ca0c4ff799
Brad Werth <bwerth@iexposure.com>
2013-10-01 11:52:48 -0500

    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PassengerBugExample::Application.config.secret_key_base = 'cdd31ef0a524b4c0c53eace7bffa0b637405831558bb6ee90b20d29853c654efdd01ace4558567c3e786e71cab62d4374d64aff06d139bc62c10cb8f397c73a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PassengerBugExample::Application.config.session_store :cookie_store, key: '_passenger_bug_example_session'
