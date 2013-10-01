2049a51c862bc229344b25bd8bead98f3f8fc283
Brad Werth <bwerth@iexposure.com>
2013-10-01 12:03:44 -0500

    add failing code

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,15 @@
 class Thing < ActiveRecord::Base
+
+
+  after_initialize :fail_strangely
+
+
+  def fail_strangely
+    dynamic_attribute = "a#{ SecureRandom.hex }".to_sym
+
+    self.class.send :attr_accessor , dynamic_attribute
+    self.class.send :validates , dynamic_attribute , presence: true
+  end
+
+
 end
